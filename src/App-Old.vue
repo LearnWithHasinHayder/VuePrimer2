3a2978825db7c8a7c3acf826f6fb0755008f091e
Hasin Hayder <countdraculla@gmail.com>
2023-12-25 22:31:17 +0600

    Code

---
+++ src/App-Old.vue
@@ -0,0 +1,51 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  components: {
+    helloworld:HelloWorld
+  },
+  setup(){
+    function sayHello(){
+        return "Hello World!"
+    }
+    return {
+      sayHello,
+      message: 'Hello Vue 3 + Vite!!!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+
+  <h1>{{ message }}</h1>
+
+  <h2> {{ sayHello() }}</h2>
+
+  <helloworld></helloworld>
+
+</template>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup>
+const message = "Hello From Component"
+</script>
+ 
+<template>
+  <h3> {{ message }}</h3>
+</template>
+ 
+<style>
+ 
+</style>
